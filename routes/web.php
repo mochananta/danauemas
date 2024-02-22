e52ec76a6698985b06343743510d385c2b3927e8
Mohammadananta <anantamohammad2003@gmail.com>
2024-02-22 20:23:49 +0700

    first

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\BeritaController;
+use App\Http\Controllers\ContactController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('user.index');
+})->name('user.index');
+
+Route::get('/about', [AboutController::class,'index'])->name('user.about');
+Route::get('/berita', [BeritaController::class,'index'])->name('user.berita');
+Route::get('/contact', [ContactController::class,'index'])->name('user.contact');
